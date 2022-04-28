04fe677eebf1a6060ede49e9ec5f9c2c3d39e475
黄继方 <50090262+huangjf1@users.noreply.github.com>
2022-04-28 18:00:46 +0800

    Add files via upload

---
+++ precache-manifest.2b9f3db656815c8e959a96accf8c5900.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "12fa046e5060c8c4bc00",
+    "url": "css/app.aa8f6edb.css"
+  },
+  {
+    "revision": "12fa046e5060c8c4bc00",
+    "url": "js/app.9742ebc1.js"
+  },
+  {
+    "revision": "69fd0da105bd60780cad",
+    "url": "css/chunk-vendors.be953b86.css"
+  },
+  {
+    "revision": "69fd0da105bd60780cad",
+    "url": "js/chunk-vendors.f881ecf8.js"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "e66ae9cb65327a3bd55010075c2ca8e0",
+    "url": "img/2.e66ae9cb.jpg"
+  },
+  {
+    "revision": "07488da54bbd6aa7bf5dd6c95ec68445",
+    "url": "img/1.07488da5.jpg"
+  },
+  {
+    "revision": "95bef9fcee59cd4fbe83c08fefaeb4c7",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "0a3edc3b6a91f3c207cb86e2ed1990c2",
+    "url": "opencvjs/opencv_js.js"
+  },
+  {
+    "revision": "525ab37827e99952d51f4f0a991da214",
+    "url": "opencvjs/opencv.js"
+  },
+  {
+    "revision": "2afddca5990816257ded7b1f249a6cf4",
+    "url": "opencvjs/opencv_js.wasm"
+  }
+];
